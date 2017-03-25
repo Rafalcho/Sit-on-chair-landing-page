eab7d67353c461e7d39f1429202e7146a49ddebf
Rafal Cholaj <raf.cholaj@gmail.com>
2017-03-25 18:16:44 +0100

    added slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-setTimeout(function(){
-  document.body.className="";
-},500);
+document.addEventListener('DOMContentLoaded', function() {
+
+  setTimeout(function() {
+    document.body.classList.remove('preload');
+  }, 500);
+
+  // slider
+
+  var leftArrow = document.querySelector('.leftArrow');
+  var rightArrow = document.querySelector('.rightArrow');
+  var sliderImages = Array.from(document.querySelectorAll('.slider li'));
+  var sliderIndex = 0;
+
+  sliderImages[sliderIndex].classList.add('visible');
+
+  leftArrow.addEventListener('click', function() {
+    sliderImages[sliderIndex].classList.remove('visible');
+
+    if (sliderIndex == 0) {
+      sliderIndex = sliderImages.length - 1;
+    } else {
+      sliderIndex--;
+    }
+
+    sliderImages[sliderIndex].classList.add('visible');
+  });
+
+  rightArrow.addEventListener('click', function() {
+    sliderImages[sliderIndex].classList.remove('visible');
+
+    if (sliderIndex == sliderImages.length - 1) {
+      sliderIndex = 0;
+    } else {
+      sliderIndex++;
+    }
+
+    sliderImages[sliderIndex].classList.add('visible');
+
+  });
+
+});
