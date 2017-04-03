9999f0e54d44fe92b712e6364225ff200ce018ec
Rafal Cholaj <raf.cholaj@gmail.com>
2017-04-03 19:32:19 +0200

    added calculator

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
   //   document.body.classList.remove('preload');
   // }, 500);
 
+  // disabling links default action
+
+  var allLinks = Array.from(document.querySelectorAll('a'));
+
+  allLinks.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+    });
+  });
+
   // hover menu
 
   var about = document.querySelector('.menu li:first-child');
@@ -51,4 +61,128 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 
+  // calculator
+
+  var listPanels = Array.from(document.querySelectorAll('.list_panel'));
+  var chooseArrows = Array.from(document.querySelectorAll('.list_arrow'));
+  var colorListItems = Array.from(document.querySelector('.color_list').children);
+  var typeListItems = Array.from(document.querySelector('.type_list').children);
+  var materialListItems = Array.from(document.querySelector('.material_list').children);
+  var transport = document.querySelector('#transport');
+  var sumSpan = document.querySelector('.sum');
+  var sumValue = 0;
+
+  // showing or hiding the list after clicking an arrow
+
+  chooseArrows.forEach(function(element) {
+    element.addEventListener('click', function() {
+
+      if (this.nextElementSibling.classList.contains('hide')) {
+        listPanels.forEach(function(element) {
+          element.classList.add('hide');
+        });
+        this.nextElementSibling.classList.remove('hide');
+      } else {
+        this.nextElementSibling.classList.add('hide');
+      }
+
+    });
+  });
+
+  // adding prices
+
+  typeListItems.forEach(function(element) {
+
+    element.addEventListener('click', function() {
+
+      var typeName = document.querySelector('h4.title');
+      var typeValue = document.querySelector('h4.type_value');
+
+      if (!typeValue.textContent) {
+
+        sumValue += parseInt(this.dataset.price);
+        sumSpan.textContent = sumValue;
+      }
+
+      typeName.textContent = this.textContent;
+      typeValue.textContent = this.dataset.price;
+
+      this.parentElement.parentElement.firstElementChild.textContent = this.textContent;
+
+      this.parentElement.classList.add('hide');
+
+      if (this.dataset.color == 'orange') {
+        document.querySelector('.chairImage').className = 'chairImage';
+        document.querySelector('.chairImage').classList.add('orangeChair');
+      } else if (this.dataset.color == 'black') {
+        document.querySelector('.chairImage').className = 'chairImage';
+        document.querySelector('.chairImage').classList.add('blackChair');
+      } else {
+        document.querySelector('.chairImage').className = 'chairImage';
+      }
+
+    });
+  });
+
+  colorListItems.forEach(function(element) {
+
+    element.addEventListener('click', function() {
+      var colorName = document.querySelector('span.color');
+      var colorValue = document.querySelector('span.color_value');
+
+      if (!colorValue.textContent) {
+
+        sumValue += parseInt(this.dataset.price);
+        sumSpan.textContent = sumValue;
+      }
+
+      colorName.textContent = this.textContent;
+      colorValue.textContent = this.dataset.price;
+
+      this.parentElement.parentElement.firstElementChild.textContent = this.textContent;
+
+      this.parentElement.classList.add('hide');
+
+    });
+  });
+
+  materialListItems.forEach(function(element) {
+
+    element.addEventListener('click', function() {
+      var materialName = document.querySelector('span.pattern');
+      var materialValue = document.querySelector('span.pattern_value');
+
+      if (!materialValue.textContent) {
+        sumValue += parseInt(this.dataset.price);
+        sumSpan.textContent = sumValue;
+      }
+
+      materialName.textContent = this.textContent;
+      materialValue.textContent = this.dataset.price;
+
+      this.parentElement.parentElement.firstElementChild.textContent = this.textContent;
+
+      this.parentElement.classList.add('hide');
+
+    });
+  });
+
+  transport.addEventListener('click', function() {
+
+      var transportName = document.querySelector('span.transport');
+      var transportValue = document.querySelector('span.transport_value');
+      if (!transport.checked) {
+        transportName.textContent = '';
+        transportValue.textContent = '';
+        sumValue -= parseInt(this.dataset.price);
+
+      } else {
+        transportName.textContent = 'Transport';
+        transportValue.textContent = this.dataset.price;
+        sumValue += parseInt(this.dataset.price);
+      }
+
+      sumSpan.textContent = sumValue;
+
+    });
 });
